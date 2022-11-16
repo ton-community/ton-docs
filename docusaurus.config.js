12d027a7fab36d974b5994c75445a10d58f85300
Dimitreee <danewdima@gmail.com>
2022-11-16 22:09:14 +0400

    update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
   title: 'The Open Network',
   tagline: 'Mass adoption. Massive distribution',
-  url: 'https://tonspace.co',
+  url: 'https://ton.org/docs/',
   staticDirectories: ['public', 'static'],
   baseUrl: '/',
   themes: [
