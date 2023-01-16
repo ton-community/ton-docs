733cd3a4a5e37f0763eed064b7e345b70b1dd3aa
sinvorgena <Pismecho@gmail.com>
2023-01-16 19:34:17 +0800

    add new buttons

---
+++ src/components/button.tsx
@@ -0,0 +1,32 @@
+import Link from '@docusaurus/Link'
+import React from 'react'
+
+// <Link
+// className="button button--secondary button--lg"
+//
+//
+//     to="/docs/intro">
+//     Серая
+// </Link>
+
+interface IButtonProps {
+    sizeType: 'lg' | 'sm'
+    href: string
+    colorType: 'primary' | 'secondary'
+    children: string
+}
+
+const Button = (props: IButtonProps) => {
+    const classNames = `button button--${props.colorType} button--${props.sizeType}`
+
+    return (
+        <Link
+            className={classNames}
+            to={props.href}
+        >
+            {props.children}
+        </Link>
+    )
+}
+
+export default Button
