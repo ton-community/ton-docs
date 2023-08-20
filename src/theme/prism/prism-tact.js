2c7cd2cb02c09cf2e5145eee54752efcd807eada
Novus Nota <68142933+novusnota@users.noreply.github.com>
2023-08-20 19:10:48 +0200

    feat: Updated Tact language highlighting for Prism.js

--- src/theme/prism/prism-tact.js
+++ src/theme/prism/prism-tact.js
@@ -1,53 +1,130 @@
-   (function (Prism) {
-    // 1. Does not start from "
-    // 2. Can start from ` and end with `, containing any character
-    // 3. Starts with underscore or { or } and have more than 1 character after it
-    // 4. Starts with letter, contains letters, numbers and underscores
-    var identifier = /(?!")(`([^`]+)`|((?=_)_|(?=\{)\{|(?=\})\}|(?![_`{}]))([^;,\[\]\(\)\s~.]+))/;
-    var string = /"[^\n"]+"[Hhcusa]?/;
-    var number = /\b([\d_]+|0x[\d_a-fA-F]+|0b[1_0]+)\b/;
+(function(Prism) {
+  Prism.languages.tact = {
+    // reserved keywords
+    'keyword': [
+      {
+        pattern: /\b(?:abstract|as|const|contract(?!:)|do|else|extend|extends|fun|get|if|import|initOf|inline|let|message(?!:)|mutates|native|override|primitive|public|repeat|return|self|struct(?!:)|trait(?!:)|until|virtual|while|with)\b/,
+      },
+      { // keyword after as
+        pattern: /(\bas\s+)\w+/,
+        lookbehind: true,
+        greedy: true,
+      },
+      { // reserved function names
+        pattern: /\b(?:init|receive|bounced|external)\b/
+      },
+    ],
 
-    Prism.languages.tact = {
-      'include': {
-        pattern: /#include(.*);/,
-        inside: {
-          'keyword': /#include/,
-          'string': string,
-          'punctuation': /;/
+    // built-in types
+    'builtin': {
+      pattern: /\b(?:Int|Bool|Address|Slice|Cell|Builder|String|StringBuilder)\b/,
+    },
+
+    // SCREAMING_SNAKE_CASE for null values and names of constants
+    'constant': [
+      {
+        pattern: /\bnull\b/,
+      },
+      {
+        pattern: /\b[A-Z][A-Z0-9_]*\b/,
+      },
+    ],
+
+    // UpperCamelCase for names of contracts, traits, structs, messages
+    'class-name': {
+      pattern: /\b[A-Z][\w]*\b/,
+    },
+
+    // native FunC functions mapping
+    'attribute': {
+      pattern: /@name/,
+      inside: {
+        'function': /.+/,
+      },
+    },
+
+    'function': {
+      pattern: /\b[\w]+(?=\()/,
+    },
+
+    'boolean': {
+      pattern: /\b(?:false|true)\b/,
+    },
+
+    'number': [
+      { // hexadecimal, case-insensitive /i
+        pattern: /\b0x[0-9a-f]+\b/i,
+      },
+      { // decimal integers
+        pattern: /\b[0-9]+\b/,
+      }
+    ],
+
+    'string': undefined,
+
+    'punctuation': {
+      pattern: /[{}[\]();,.:?]/,
+    },
+
+    'comment': [
+      { // single-line
+        pattern: /(^|[^\\:])\/\/.*/,
+        lookbehind: true,
+        greedy: true,
+      },
+      { // multi-line
+        pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+        lookbehind: true,
+        greedy: true,
+      }
+    ],
+
+    'operator': {
+      'pattern': /![!=]?|\*|\/|%|-|\+|==?|[<>]=?|<<|>>|\|\|?|&&?/,
+    },
+
+  };
+
+  // strings, made this way to not collide with other entities
+  Prism.languages.insertBefore('tact', 'string', {
+    'string-literal': {
+      pattern: /(?:(")(?:\\.|(?!\1)[^\\\r\n])*\1(?!\1))/,
+      greedy: true,
+      inside: {
+        'string': {
+          pattern: /[\s\S]+/,
         },
       },
-      'pragma': {
-        pattern: /#pragma(.*);/,
-        inside: {
-          'keyword': /#pragma|not-version|version/,
-          'number': /(\d+)(.\d+)?(.\d+)?/,
-          'operator': [/>=/, /<=/, /=/, />/, /</, /\^/],
-          'punctuation': /;/
-        }
+    },
+  });
+
+  // map and bounced message generic type modifiers
+  Prism.languages.insertBefore('tact', 'keyword', {
+    'generics': {
+      pattern: /(?:\b(?:map|bounced)\b<[^\\\r\n]*>)/,
+      greedy: true,
+      inside: {
+        'builtin': [
+          Prism.languages['tact']['builtin'],
+          {
+            pattern: /\b(?:map(?=<)|bounced(?=<))\b/
+          },
+        ],
+        'class-name': Prism.languages['tact']['class-name'],
+        'punctuation': {
+          pattern: /[<>(),.?]/,
+        },
+        'keyword': [
+          {
+            pattern: /\bas\b/,
+          },
+          {
+            pattern: /(\bas\s+)\w+/,
+            lookbehind: true,
+            greedy: true,
+          },
+        ],
       },
-
-      'comment': [
-        {
-          pattern: /;;.*/,
-          lookbehind: true,
-          greedy: true
-        },
-        {
-          pattern: /\{-[\s\S]*?(?:-\}|$)/,
-          lookbehind: true,
-          greedy: true
-        },
-      ],
-
-      'keyword': /\b(?:_(?=\s*:)|asm|const|do|else|elseif|elseifnot|forall|global|if|ifnot|impure|inline|inline_ref|method_id|repeat|return|until|while)\b/,
-      'boolean': /\b(?:false|true)\b/,
-      'builtin': /\b(?:_|builder|cell|cont|int|slice|tuple|var)\b/,
-
-      'string': string,
-      'number': number,
-      'variable': identifier,
-
-      'operator': /(<=>|>=|<=|!=|==|~>>=|~>>|\/%|\^%=|\^%|~%|\^\/=|\^\/|~\/=|~\/|\+=|-=|\*=|\/=|%=|<<=|>>=|\^>>=|\^>>|&=|>>|<<|\^=|\|=|\^|=|~|\/|%|-|\*|\+|>|<|&|\||:|\?)/,
-      'punctuation': /[\.;\(\),\[\]~\{\}]/,
-    };
-  }(Prism));
+    },
+  });
+}(Prism));
