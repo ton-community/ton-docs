92e4d97edb44fee131f17a25cf3545f33788a9e1
AlexG <39581753+Reveloper@users.noreply.github.com>
2024-04-28 13:26:12 +0900

    doc-update-3
    * Added new address verification page
    * Added new TON Academy section for educational materials navigation
    * minor fixes and improvements

--- sidebars.js
+++ sidebars.js
@@ -23,6 +23,25 @@ const sidebars = {
     },
     {
       'type': 'html',
+      'value': '<span class=\'menu__link\'><b><small> TON Academy </small></b></span>',
+    },
+    {
+      type: 'category',
+      label: 'Learn',
+      items: [
+        {
+          type: 'doc',
+          label: 'Overview',
+          id: 'learn/academy/academy-overview',
+        },
+      ]
+    },
+    {
+      'type': 'html',
+      'value': '<hr/>',
+    },
+    {
+      'type': 'html',
       'value': '<span class=\'menu__link\'><b><small> TON Concepts </small></b></span>',
     },
     {
