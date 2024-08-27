666a957fd1c4fba52bbc69ad5887a813f409afce
PixelPlex Dev team <10460630+pixelplex@users.noreply.github.com>
2024-08-27 09:58:10 +0300

    Added info run docker (#611)
    * add info run in docker
    
    * change docker repository mytonctrl
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    * Update docs/participate/run-nodes/run-docker.md
    
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>
    
    ---------
    
    Co-authored-by: Sergey Andreev <s.andreev@pixelplex.io>
    Co-authored-by: AlexG <39581753+reveloper@users.noreply.github.com>
    Co-authored-by: Dr. Awesome Doge <doge@ton.org>

--- sidebars.js
+++ sidebars.js
@@ -669,16 +669,16 @@ const sidebars = {
       items: [
         'participate/nodes/node-types',
         'participate/run-nodes/mytonctrl',
+        'participate/run-nodes/run-docker',
         'participate/run-nodes/full-node',
         'participate/run-nodes/enable-liteserver-node',
         'participate/run-nodes/become-validator',
         'participate/run-nodes/archive-node',
         'participate/run-nodes/nodes-troubleshooting',
-        //'participate/run-nodes/liteserver',
-        //'participate/nodes/nodes-faq',
         'participate/nodes/node-maintenance-and-security',
         'participate/run-nodes/local-ton',
         'participate/run-nodes/faq',
+
       ],
     },
     {
