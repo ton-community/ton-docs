078e116a7dba6d1994896134bfe0cdc52d59114b
sansx <xiathxth@gmail.com>
2024-08-30 16:27:30 +0800

    Add vue section (#738)
    * add vue section
    
    * cut numbers from header
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -399,6 +399,11 @@ const sidebars = {
             },
             {
               type: 'doc',
+              id: 'develop/dapps/ton-connect/vue',
+              label: 'Vue Apps',
+            },
+            {
+              type: 'doc',
               id: 'develop/dapps/ton-connect/web',
               label: 'HTML/JS Apps',
             },
