9868296e49300c4576e246e8ff35099a48302e09
Aliaksandr Bahdanau <a.bahdanau@pixelplex.io>
2024-10-17 12:28:20 +0300

    fix: fix move script

--- mv.py
+++ mv.py
@@ -9,24 +9,28 @@ redirect_data = []
 
 class MoveEventHandler(FileSystemEventHandler):
     def on_moved(self, event):
+        if '.git' in event.src_path or '.git' in event.dest_path:
+            return
+
         if event.is_directory:
             print(f"Directory moved from {event.src_path} to {event.dest_path}")
         else:
             print(f"File moved from {event.src_path} to {event.dest_path}")
 
         repo_path = os.getcwd()
-       
+
         source = get_redirect(event.src_path, repo_path)
         destination = get_redirect(event.dest_path, repo_path)
 
-        print(source)
-        print(destination)
+        print("from_r:",source)
+        print("to_r:", destination)
 
         # path_r = destination.split("/")[1]
+        json_file = f"./redirects/redirects.json"
         file_extensions = ['.mdx', '.md']
 
         replace_in_repo(repo_path, source, destination, file_extensions)
-        redirect(source, destination)
+        redirect(json_file, source, destination)
 
 
 def start_monitoring(path):
@@ -42,13 +46,13 @@ def start_monitoring(path):
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
-        
+
     observer.join()
 
 def find_markdown_links(text):
     md_link_pattern = r'\[([^\]]+)\]\(([^#)]+)([^)]*)\)'
     matches = re.findall(md_link_pattern, text)
-    
+
     return matches
 
 def replace_word_in_file(file_path, target, replacement):
@@ -70,53 +74,57 @@ def replace_word_in_file(file_path, target, replacement):
 
 def replace_in_repo(repo_path, target, replacement, file_extensions=None):
     for root, dirs, files in os.walk(repo_path):
-        print(root, repo_path)
-        # if root == repo_path:
-        #     for file_name in files:
-        #         if file_name == "navbar.js" or file_name == "redirects.js":
-        #             file_path = os.path.join(root, file_name)
-        #             replace_word_in_file(file_path, target, replacement)
-        #     continue
+        if '.git' in root or 'i18n' in root:
+            continue
 
-        # path_list = root.split(repo_path)
-        # if len(path_list) > 1 and path_list[1] == "/sidebars":
-        #     for file_name in files:
-        #         file_path = os.path.join(root, file_name)
-        #         replace_word_in_file(file_path, target, replacement)
-        #     continue
+        if root == repo_path:
+            for file_name in files:
+                if file_name == "navbar.js" or file_name == "redirects.js":
+                    file_path = os.path.join(root, file_name)
+                    replace_word_in_file(file_path, target, replacement)
+            continue
 
-        # for file_name in files:
-        #     if file_extensions:
-        #         if not any(file_name.endswith(ext) for ext in file_extensions):
-        #             continue
-            
-        #     if root.find("node_modules") != -1:
-        #         continue
+        path_list = root.split(repo_path)
+        if len(path_list) > 1 and path_list[1] == "/sidebars":
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                replace_word_in_file(file_path, target, replacement)
+            continue
 
-        #     file_path = os.path.join(root, file_name)
-        #     replace_word_in_file(file_path, target, replacement)
+        for file_name in files:
+            if file_extensions:
+                if not any(file_name.endswith(ext) for ext in file_extensions):
+                    continue
+
+            if root.find("node_modules") != -1:
+                continue
+
+            file_path = os.path.join(root, file_name)
+            replace_word_in_file(file_path, target, replacement)
 
 def get_redirect(path, repo):
-    res = os.path.splitext(path)[0]
+    res = os.path.splitext(path)
+    res = res[0]
     spl = res.split(repo)
-    res = spl[1].split('/docs')[1]
+    res = spl[1].split('/docs', 1)
+    res = res[1]
     return res
 
-def redirect(from_r, to_r):
+def redirect(json_file, from_r, to_r):
     obj = {
         "from": from_r,
         "to": to_r
     }
 
-    # with open(json_file, 'r') as file:
-    #     data = json.load(file)
-        
-    redirect_data.append(obj)
+    with open(json_file, 'r') as file:
+        data = json.load(file)
 
-    # with open(json_file, 'w') as file:
-    #     json.dump(obj, file, indent=4)
+    data.append(obj)
 
-    
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
 if __name__ == "__main__":
     repo_path = os.getcwd()
 
@@ -131,10 +139,6 @@ if __name__ == "__main__":
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        print("STOPPED")
-        json_file = f"./redirects/redirects.json"
-        with open(json_file, 'w') as file:
-            json.dump(redirect_data, file, indent=4)
         observer.stop()
 
     observer.join()
