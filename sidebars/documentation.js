3194a98b2b776719622525091ed4b267294f5dd2
Gleb Karavatski <g.karavatski@pixelplex.io>
2024-11-25 11:07:48 +0300

    add nodes min requirements to the nodes doc main page

--- sidebars/documentation.js
+++ sidebars/documentation.js
@@ -189,23 +189,6 @@ module.exports = [
     'type': 'html',
     'value': '<span class=\'menu__link\'><b><small> Infrastructure Documentation </small></b></span>',
   },
-  'v3/documentation/infra/minter-flow',
-  {
-    type: 'category',
-    label: 'Cross-chain Bridges',
-    items: [
-      {
-        type: 'doc',
-        label: 'Overview',
-        id: 'v3/documentation/infra/crosschain/overview',
-      },
-      {
-        type: 'doc',
-        label: 'Bridges Addresses',
-        id: 'v3/documentation/infra/crosschain/bridge-addresses',
-      },
-    ],
-  },
   {
     type: 'category',
     label: 'Blockchain Nodes',
@@ -232,6 +215,23 @@ module.exports = [
           },
           'v3/documentation/infra/nodes/validation/collators',
         ],
+      },
+    ],
+  },
+  'v3/documentation/infra/minter-flow',
+  {
+    type: 'category',
+    label: 'Cross-chain Bridges',
+    items: [
+      {
+        type: 'doc',
+        label: 'Overview',
+        id: 'v3/documentation/infra/crosschain/overview',
+      },
+      {
+        type: 'doc',
+        label: 'Bridges Addresses',
+        id: 'v3/documentation/infra/crosschain/bridge-addresses',
       },
     ],
   },
