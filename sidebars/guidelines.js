4df19a45bbc61b4e80a8acfec71a62d162be7ca1
AlexG <39581753+reveloper@users.noreply.github.com>
2025-03-10 20:02:57 +0900

    Weekly update 25 10 (#997)
    * refactor_guidelines_sidebar
    
    * refactor_guideline_overview
    
    * cut_leagacy_pages
    
    * Update guidelines.mdx
    
    Proofread.
    
    * split_tables

--- sidebars/guidelines.js
+++ sidebars/guidelines.js
@@ -30,82 +30,76 @@ module.exports = [
     ],
   },
   {
-    'type': 'html',
-    'value': '<hr/>',
-  },
-  {
-    'type': 'html',
-    'value': '<span class=\'menu__link\'><b><small> Smart Contracts Guidelines </small></b></span>',
-  },
-  'v3/guidelines/smart-contracts/guidelines',
-  'v3/guidelines/smart-contracts/get-methods',
-  {
-    type: 'doc',
-    label: 'Transaction fees calculation',
-    id: 'v3/guidelines/smart-contracts/fee-calculation',
-  },
-  {
     type: 'category',
-    label: 'Testing',
+    label: ' Smart contracts guidelines',
     items: [
-      'v3/guidelines/smart-contracts/testing/overview',
-      'v3/guidelines/smart-contracts/testing/writing-test-examples',
-    ],
-  },
-  {
-    type: 'category',
-    label: 'Security Measures',
-    items: [
-      'v3/guidelines/smart-contracts/security/overview',
-      'v3/guidelines/smart-contracts/security/secure-programming',
-      'v3/guidelines/smart-contracts/security/things-to-focus',
-      'v3/guidelines/smart-contracts/security/ton-hack-challenge-1',
-      'v3/guidelines/smart-contracts/security/random-number-generation',
-      'v3/guidelines/smart-contracts/security/random',
-    ],
-  },
-  {
-    type: 'category',
-    label: 'How to',
-    items: [
+      'v3/guidelines/smart-contracts/guidelines',
+      'v3/guidelines/smart-contracts/get-methods',
+      {
+        type: 'doc',
+        label: 'Transaction fees calculation',
+        id: 'v3/guidelines/smart-contracts/fee-calculation',
+      },
       {
         type: 'category',
-        label: 'Compile from Sources',
+        label: 'Testing',
+        items: [
+          'v3/guidelines/smart-contracts/testing/overview',
+          'v3/guidelines/smart-contracts/testing/writing-test-examples',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'Security measures',
+        items: [
+          'v3/guidelines/smart-contracts/security/overview',
+          'v3/guidelines/smart-contracts/security/secure-programming',
+          'v3/guidelines/smart-contracts/security/things-to-focus',
+          'v3/guidelines/smart-contracts/security/ton-hack-challenge-1',
+          'v3/guidelines/smart-contracts/security/random-number-generation',
+          'v3/guidelines/smart-contracts/security/random',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'How to',
         items: [
           {
-            type: 'doc',
-            label: 'Compilation Instructions',
-            id: 'v3/guidelines/smart-contracts/howto/compile/compilation-instructions',
+            type: 'category',
+            label: 'Compile from sources',
+            items: [
+              {
+                type: 'doc',
+                label: 'Compilation instructions',
+                id: 'v3/guidelines/smart-contracts/howto/compile/compilation-instructions',
+              },
+              {
+                type: 'doc',
+                label: 'Instructions for low-memory machines',
+                id: 'v3/guidelines/smart-contracts/howto/compile/instructions-low-memory',
+              },
+            ],
           },
+          'v3/guidelines/smart-contracts/howto/multisig',
+          'v3/guidelines/smart-contracts/howto/multisig-js',
+          'v3/guidelines/smart-contracts/howto/airdrop-claim-best-practice',
+          'v3/guidelines/smart-contracts/howto/shard-optimization',
+          'v3/guidelines/smart-contracts/howto/wallet',
+          'v3/guidelines/smart-contracts/howto/nominator-pool',
+          'v3/guidelines/smart-contracts/howto/single-nominator-pool',
           {
-            type: 'doc',
-            label: 'Instructions for low-memory machines',
-            id: 'v3/guidelines/smart-contracts/howto/compile/instructions-low-memory',
+            type: 'link',
+            label: 'How to shard your TON smart contract and why',
+            href: 'https://blog.ton.org/how-to-shard-your-ton-smart-contract-and-why-studying-the-anatomy-of-tons-jettons',
           },
         ],
       },
-      'v3/guidelines/smart-contracts/howto/multisig',
-      'v3/guidelines/smart-contracts/howto/multisig-js',
-      'v3/guidelines/smart-contracts/howto/airdrop-claim-best-practice',
-      'v3/guidelines/smart-contracts/howto/shard-optimization',
-      'v3/guidelines/smart-contracts/howto/wallet',
-      'v3/guidelines/smart-contracts/howto/nominator-pool',
-      'v3/guidelines/smart-contracts/howto/single-nominator-pool',
-      {
-        type: 'link',
-        label: 'How to shard your TON smart contract and why',
-        href: 'https://blog.ton.org/how-to-shard-your-ton-smart-contract-and-why-studying-the-anatomy-of-tons-jettons',
-      },
-    ],
+    ]
   },
   {
-    'type': 'html',
-    'value': '<hr/>',
-  },
-  {
-    'type': 'html',
-    'value': '<span class=\'menu__link\'><b><small> DApps Guidelines </small></b></span>',
-  },
+    type: 'category',
+    label: ' DApps guidelines',
+    items: [
   'v3/guidelines/dapps/overview',
   'v3/guidelines/dapps/cookbook',
   {
@@ -121,33 +115,33 @@ module.exports = [
   },
   {
     type: 'category',
-    label: 'Tutorials & Examples',
+    label: 'Tutorials & examples',
     items: [
       'v3/guidelines/dapps/apis-sdks/api-keys',
       'v3/guidelines/dapps/apis-sdks/getblock-ton-api',
       {
         type: 'doc',
         id: 'v3/guidelines/dapps/tutorials/nft-minting-guide',
-        label: 'NFT Minting Guide',
+        label: 'NFT minting guide',
       },
       {
         type: 'doc',
         id: 'v3/guidelines/dapps/tutorials/mint-your-first-token',
-        label: 'Mint Your First Token',
+        label: 'Mint your first token',
       },
       {
         type: 'doc',
         id: 'v3/guidelines/dapps/tutorials/zero-knowledge-proofs',
-        label: 'Zero-Knowledge Proofs',
+        label: 'Zero-Knowledge proofs',
       },
       {
         type: 'doc',
         id: 'v3/guidelines/dapps/tutorials/web3-game-example',
-        label: 'Web3 Game Example',
+        label: 'Web3 game example',
       },
       {
         type: 'category',
-        label: 'Telegram Bot Examples',
+        label: 'Telegram bot examples',
         items: [
           'v3/guidelines/dapps/tutorials/telegram-bot-examples/accept-payments-in-a-telegram-bot',
           'v3/guidelines/dapps/tutorials/telegram-bot-examples/accept-payments-in-a-telegram-bot-2',
@@ -173,7 +167,7 @@ module.exports = [
       },
       {
         type: 'category',
-        label: 'Tutorials & Examples',
+        label: 'Tutorials & examples',
         items: [
           'v3/guidelines/dapps/tma/tutorials/step-by-step-guide',
           'v3/guidelines/dapps/tma/tutorials/app-examples',
@@ -186,171 +180,168 @@ module.exports = [
   },
   {
     type: 'category',
-    label: 'Advanced Asset Processing',
+    label: 'Advanced asset processing',
     items: [
-      'v3/guidelines/dapps/asset-processing/payments-processing', // TODO: divide
-      'v3/guidelines/dapps/asset-processing/jettons', // TODO: divide
-      'v3/guidelines/dapps/asset-processing/mintless-jettons', // TODO: divide
+      'v3/guidelines/dapps/asset-processing/payments-processing',
+      'v3/guidelines/dapps/asset-processing/jettons',
+      'v3/guidelines/dapps/asset-processing/mintless-jettons',
       'v3/guidelines/dapps/asset-processing/compressed-nfts',
       'v3/guidelines/dapps/asset-processing/mass-mint-tools',
       {
         type: 'category',
-        label: 'NFT Processing',
+        label: 'NFT processing',
         items: [
-          'v3/guidelines/dapps/asset-processing/nft-processing/nfts', // TODO: divide
+          'v3/guidelines/dapps/asset-processing/nft-processing/nfts',
           'v3/guidelines/dapps/asset-processing/nft-processing/metadata-parsing',
         ],
       },
     ],
   },
-  {
-    'type': 'html',
-    'value': '<hr/>',
-  },
-  {
-    'type': 'html',
-    'value': '<span class=\'menu__link\'><b><small> Blockchain Nodes Guidelines </small></b></span>',
-  },
-  'v3/guidelines/nodes/overview',
-  {
-    type: 'category',
-    label: 'Running Nodes',
-    items: [
-      'v3/guidelines/nodes/running-nodes/archive-node',
-      'v3/guidelines/nodes/running-nodes/full-node',
-      'v3/guidelines/nodes/running-nodes/liteserver-node',
-      'v3/guidelines/nodes/running-nodes/validator-node',
-      'v3/guidelines/nodes/running-nodes/staking-with-nominator-pools',
-      'v3/guidelines/nodes/running-nodes/run-mytonctrl-docker',
-      'v3/guidelines/nodes/running-nodes/running-a-local-ton',
-      'v3/guidelines/nodes/running-nodes/secure-guidelines',
-    ],
+  ]
   },
   {
     type: 'category',
-    label: 'Maintenance Guidelines',
+    label: 'Blockchain nodes guidelines',
     items: [
-      'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-backup-restore',
-      'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-validator-standby',
-      'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-private-alerting',
-      'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-prometheus',
-      'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-remote-controller'
-    ],
-  },
-  'v3/guidelines/nodes/custom-overlays',
-  'v3/guidelines/nodes/nodes-troubleshooting',
-  'v3/guidelines/nodes/node-maintenance-and-security',
-  'v3/guidelines/nodes/monitoring/performance-monitoring',
-  'v3/guidelines/nodes/persistent-states',
-  'v3/guidelines/nodes/faq',
-  {
-    'type': 'html',
-    'value': '<hr/>',
-  },
-  {
-    'type': 'html',
-    'value': '<span class=\'menu__link\'><b><small> Integrate with TON </small></b></span>',
-  },
-  'v3/guidelines/ton-connect/overview',
-  {
-    type: 'doc',
-    id: 'v3/guidelines/ton-connect/wallet',
-  },
-  {
-    type: 'category',
-    label: 'Frameworks',
-    items: [
+    'v3/guidelines/nodes/overview',
+    {
+      type: 'category',
+      label: 'Running nodes',
+      items: [
+        'v3/guidelines/nodes/running-nodes/archive-node',
+        'v3/guidelines/nodes/running-nodes/full-node',
+        'v3/guidelines/nodes/running-nodes/liteserver-node',
+        'v3/guidelines/nodes/running-nodes/validator-node',
+        'v3/guidelines/nodes/running-nodes/staking-with-nominator-pools',
+        'v3/guidelines/nodes/running-nodes/run-mytonctrl-docker',
+        'v3/guidelines/nodes/running-nodes/running-a-local-ton',
+        'v3/guidelines/nodes/running-nodes/secure-guidelines',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Maintenance guidelines',
+      items: [
+        'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-backup-restore',
+        'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-validator-standby',
+        'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-private-alerting',
+        'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-prometheus',
+        'v3/guidelines/nodes/maintenance-guidelines/mytonctrl-remote-controller'
+      ],
+    },
+    'v3/guidelines/nodes/custom-overlays',
+    'v3/guidelines/nodes/nodes-troubleshooting',
+    'v3/guidelines/nodes/node-maintenance-and-security',
+    'v3/guidelines/nodes/monitoring/performance-monitoring',
+    'v3/guidelines/nodes/persistent-states',
+    'v3/guidelines/nodes/faq',
+    ]
+    },
+    {
+      type: 'category',
+      label: 'Integrate with TON',
+      items: [
+      'v3/guidelines/ton-connect/overview',
       {
         type: 'doc',
-        id: 'v3/guidelines/ton-connect/frameworks/react',
-        label: 'React Apps',
+        id: 'v3/guidelines/ton-connect/wallet',
       },
       {
-        type: 'doc',
-        id: 'v3/guidelines/ton-connect/frameworks/vue',
-        label: 'Vue Apps',
+        type: 'category',
+        label: 'Frameworks',
+        items: [
+          {
+            type: 'doc',
+            id: 'v3/guidelines/ton-connect/frameworks/react',
+            label: 'React Apps',
+          },
+          {
+            type: 'doc',
+            id: 'v3/guidelines/ton-connect/frameworks/vue',
+            label: 'Vue Apps',
+          },
+          {
+            type: 'doc',
+            id: 'v3/guidelines/ton-connect/frameworks/web',
+            label: 'HTML/JS Apps',
+          },
+        ],
       },
       {
-        type: 'doc',
-        id: 'v3/guidelines/ton-connect/frameworks/web',
-        label: 'HTML/JS Apps',
-      },
-    ],
-  },
-  {
-    type: 'category',
-    label: 'Guidelines',
-    items: [
-      'v3/guidelines/ton-connect/guidelines/how-ton-connect-works',
-      'v3/guidelines/ton-connect/guidelines/developers',
-      'v3/guidelines/ton-connect/guidelines/creating-manifest',
-      'v3/guidelines/ton-connect/guidelines/preparing-messages',
-      'v3/guidelines/ton-connect/guidelines/sending-messages',
-      'v3/guidelines/ton-connect/guidelines/verifying-signed-in-users',
-      'v3/guidelines/ton-connect/guidelines/integration-with-javascript-sdk',
-    ],
-  },
-  {
-    type: 'category',
-    label: 'Advanced',
-    items: [
-      {
-        type: 'link',
-        label: 'Protocol specification',
-        href: 'https://github.com/ton-blockchain/ton-connect',
+        type: 'category',
+        label: 'Guidelines',
+        items: [
+          'v3/guidelines/ton-connect/guidelines/how-ton-connect-works',
+          'v3/guidelines/ton-connect/guidelines/developers',
+          'v3/guidelines/ton-connect/guidelines/creating-manifest',
+          'v3/guidelines/ton-connect/guidelines/preparing-messages',
+          'v3/guidelines/ton-connect/guidelines/sending-messages',
+          'v3/guidelines/ton-connect/guidelines/verifying-signed-in-users',
+          'v3/guidelines/ton-connect/guidelines/integration-with-javascript-sdk',
+        ],
       },
       {
-        type: 'link',
-        label: 'Wallets List',
-        href: 'https://github.com/ton-blockchain/wallets-list',
+        type: 'category',
+        label: 'Advanced',
+        items: [
+          {
+            type: 'link',
+            label: 'Protocol specification',
+            href: 'https://github.com/ton-blockchain/ton-connect',
+          },
+          {
+            type: 'link',
+            label: 'Wallets List',
+            href: 'https://github.com/ton-blockchain/wallets-list',
+          },
+        ],
       },
-    ],
+    {
+      type: 'category',
+      label: 'Business',
+      items: [
+        'v3/guidelines/ton-connect/business/ton-connect-for-business',
+        'v3/guidelines/ton-connect/business/ton-connect-for-security',
+        'v3/guidelines/ton-connect/business/ton-connect-comparison',
+      ],
+    }
+  ]
   },
-  {
-    type: 'category',
-    label: 'Business',
-    items: [
-      'v3/guidelines/ton-connect/business/ton-connect-for-business',
-      'v3/guidelines/ton-connect/business/ton-connect-for-security',
-      'v3/guidelines/ton-connect/business/ton-connect-comparison',
-    ],
-  },
-  {
-    'type': 'html',
-    'value': '<hr/>',
-  },
-  {
-    'type': 'html',
-    'value': '<span class=\'menu__link\'><b><small> Web3 Guidelines </small></b></span>',
-  },
-  'v3/guidelines/web3/overview',
+
   {
     'type': 'category',
-    'label': 'TON DNS',
+    'label': 'Web3 guidelines',
     'items': [
-      'v3/guidelines/web3/ton-dns/dns',
-      'v3/guidelines/web3/ton-dns/subresolvers',
-    ],
-  },
-  {
-    'type': 'category',
-    'label': 'TON Proxy & Sites',
-    'items': [
-      'v3/guidelines/web3/ton-proxy-sites/how-to-run-ton-site',
-      'v3/guidelines/web3/ton-proxy-sites/ton-sites-for-applications',
-      'v3/guidelines/web3/ton-proxy-sites/connect-with-ton-proxy',
-      'v3/guidelines/web3/ton-proxy-sites/how-to-open-any-ton-site',
-      'v3/guidelines/web3/ton-proxy-sites/site-and-domain-management',
-      'v3/guidelines/web3/ton-proxy-sites/running-your-own-ton-proxy',
-    ],
-  },
-  {
-    'type': 'category',
-    'label': 'TON Storage',
-    'items': [
-      'v3/guidelines/web3/ton-storage/storage-daemon',
-      'v3/guidelines/web3/ton-storage/storage-provider',
-      'v3/guidelines/web3/ton-storage/storage-faq',
-    ],
+      'v3/guidelines/web3/overview',
+      {
+        'type': 'category',
+        'label': 'TON DNS',
+        'items': [
+          'v3/guidelines/web3/ton-dns/dns',
+          'v3/guidelines/web3/ton-dns/subresolvers',
+        ],
+      },
+      {
+        'type': 'category',
+        'label': 'Proxy & sites',
+        'items': [
+          'v3/guidelines/web3/ton-proxy-sites/how-to-run-ton-site',
+          'v3/guidelines/web3/ton-proxy-sites/ton-sites-for-applications',
+          'v3/guidelines/web3/ton-proxy-sites/connect-with-ton-proxy',
+          'v3/guidelines/web3/ton-proxy-sites/how-to-open-any-ton-site',
+          'v3/guidelines/web3/ton-proxy-sites/site-and-domain-management',
+          'v3/guidelines/web3/ton-proxy-sites/running-your-own-ton-proxy',
+        ],
+      },
+      {
+        'type': 'category',
+        'label': 'TON Storage',
+        'items': [
+          'v3/guidelines/web3/ton-storage/storage-daemon',
+          'v3/guidelines/web3/ton-storage/storage-provider',
+          'v3/guidelines/web3/ton-storage/storage-faq',
+        ],
+      },
+    ]
   },
 ];
