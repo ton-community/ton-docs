fb831f7c54f795a4d67cc198680554c8ea4e30b8
Ilya Egorov <ilya.e@ton.org>
2025-06-18 11:27:41 +0300

    Add ko locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const getEnvLangConfig = () => {
 
   const langArray = process.env.TARGET_LANGS
     ? process.env.TARGET_LANGS.split(",")
-    : ["mandarin", "ru"];
+    : ["ko"];
   // : ["mandarin", "ru", "ko", "pl", "uk", "ja"];
 
   const locales = Array.from(new Set([defaultLocale, ...langArray]));
@@ -23,6 +23,10 @@ const getEnvLangConfig = () => {
       'mandarin': {
         label: '简体中文',
         path: "zh-CN",
+      },
+      ko: {
+        label: "Korean",
+        path: "ko",
       },
     },
   };
