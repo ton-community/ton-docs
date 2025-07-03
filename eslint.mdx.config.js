fd23c96e73274231d3485648d2c3f450d7594203
klpx <alex@arigativa.ru>
2025-07-03 15:21:33 +0300

    enforce mdx file extension for documentation files #1303 (#1304)

--- eslint.mdx.config.js
+++ eslint.mdx.config.js
@@ -4,6 +4,8 @@ const {
 } = require("eslint/config");
 
 const mdx = require('eslint-plugin-mdx');
+const anyParser = require('any-eslint-parser');
+const regexRules = require('eslint-plugin-regex');
 
 module.exports = defineConfig([
   {
@@ -15,6 +17,27 @@ module.exports = defineConfig([
     processor: mdx.createRemarkProcessor({
       lintCodeBlocks: false,
     }),
+  },
+  {
+    languageOptions: {
+      parser: anyParser,
+    },
+    plugins: {
+      'regex': regexRules,
+    },
+    files: [
+      'docs/**/*.md',
+      'src/pages/**/*.md',
+    ],
+    rules: {
+      "regex/invalid": [
+        "error", [{
+          "message": "plain .md files are not allowed, rename it with .mdx extension",
+          "regex": ".*",
+          flags: 'su'
+        }]
+      ],
+    }
   },
   globalIgnores([
     "./.docusaurus", "./build", "./node_modules",
