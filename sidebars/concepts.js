c857aca6c8f72670ca6f95cc13f666d29afd28b6
aigerimu <89766357+aigerimu@users.noreply.github.com>
2025-08-15 04:13:34 +0500

    upd

--- sidebars/concepts.js
+++ sidebars/concepts.js
@@ -17,7 +17,6 @@ module.exports = [
     label: 'TON Blockchain',
     items: [
       'v3/concepts/dive-into-ton/ton-blockchain/blockchain-of-blockchains',
-      'v3/concepts/dive-into-ton/ton-blockchain/accounts',
       'v3/concepts/dive-into-ton/ton-blockchain/smart-contract-addresses',
       'v3/concepts/dive-into-ton/ton-blockchain/cells-as-data-storage',
       'v3/concepts/dive-into-ton/ton-blockchain/ton-networking',
